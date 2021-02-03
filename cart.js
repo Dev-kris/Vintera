221f5b821fb866a9aceee3d17fbc2d45f04c1dbd
Dev-kris <61651282+Dev-kris@users.noreply.github.com>
2021-02-03 20:02:01 +0100

    added ajax post to cart.js added BS5 form validation on cart.html
    cart.js:
    removed non-used code
    added callbacks to functions to assure timing of async functions.
    added form validation using bootstrap 5 invalid-tooltip
    created postOrder function
    added ajax post method
    added test alert to onload to confirm data structure and api are correct
    
    TODO:
    add order confirmation page.

--- cart.js
+++ cart.js
@@ -8,7 +8,7 @@ async function buildCartHTML() {
     for (var i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i);
         let cameraKeyToRemove = key;
-        
+
         const response = await fetch(api_url);
         const data = await response.json();
 
@@ -19,7 +19,6 @@ async function buildCartHTML() {
         const tableDataDivImg = document.createElement('a');
         const tableDataDivDetails = document.createElement('div');
         const tableDataQty = document.createElement('td');
-        //const tableDataQtyInput = document.createElement('input');
         const tableDataPrice = document.createElement('td');
         const tableDataSubtotal = document.createElement('td');
         const removeButton = document.createElement('td');
@@ -33,17 +32,16 @@ async function buildCartHTML() {
         tableDataDivImg.href = 'camera.html' + '?id=' + key;
         tableDataDivDetails.id = 'camera-details' + [i];
         tableDataQty.id = 'table-data-qty' + [i];
-       // tableDataQtyInput.id = 'camera-qty' + [i];
         tableDataPrice.id = 'camera-price' + [i];
         tableDataSubtotal.id = 'camera-subtotal' + [i];
         removeButton.id = 'remove-td' + [i];
         removeButtonBtn.id = 'remove-tdd' + [i];
-        
+
         // add button to each product to allow removal
         removeButtonBtn.className = 'btn filter-red btn-remove-cart';
         removeButton.addEventListener('click', function () {
             localStorage.removeItem(cameraKeyToRemove);
-           location.reload();
+            location.reload();
         });
 
 
@@ -54,13 +52,13 @@ async function buildCartHTML() {
         document.getElementById('cart-camera-details' + [i]).appendChild(tableDataDivImg);
         document.getElementById('cart-camera-details' + [i]).appendChild(tableDataDivDetails);
         document.getElementById('table-row' + [i]).appendChild(tableDataQty);
-        //document.getElementById('table-data-qty' + [i]).appendChild(tableDataQtyInput);
         document.getElementById('table-row' + [i]).appendChild(tableDataPrice);
         document.getElementById('table-row' + [i]).appendChild(tableDataSubtotal);
         document.getElementById('table-row' + [i]).appendChild(removeButton);
         document.getElementById('remove-td' + [i]).appendChild(removeButtonBtn);
 
     }
+    getCameraImage();
 }
 async function getCameraImage() {
 
@@ -76,6 +74,8 @@ async function getCameraImage() {
         cameraImage.className = 'thumbnail-format';
         document.getElementById('cart-thumbnail' + [i]).appendChild(cameraImage);
     }
+    getCameraDetails();
+
 }
 
 
@@ -102,7 +102,10 @@ async function getCameraDetails() {
         document.getElementById('camera-subtotal' + [i]).innerHTML = '$ ' + lineSubtotal;
 
     }
+    getCartTotals();
+
 }
+let testTotal = 0;
 async function getCartTotals() {
 
     let cartSubtotal = 0;
@@ -113,24 +116,91 @@ async function getCartTotals() {
         const data = await response.json();
 
         const qty = parseInt(localStorage.getItem(key));
-        const subTotal = qty * (data.price / 100);
+        const subtotal = qty * (data.price / 100);
 
-        cartSubtotal += subTotal;
+        cartSubtotal += subtotal;
     }
     let cartTax = cartSubtotal * .2;
-    let cartTotal = cartSubtotal + cartTax;
+    var cartTotal = cartSubtotal + cartTax;
+    testTotal += cartTotal;
 
     document.getElementById('subtotal-data').innerHTML = '$ ' + cartSubtotal.toFixed(2);
     document.getElementById('tax-data').innerHTML = '$ ' + cartTax.toFixed(2);
     document.getElementById('total-data').innerHTML = '$ ' + cartTotal.toFixed(2);
-
 }
+console.log('The test total is ' + testTotal);
+// form validation
+
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+// post cart and form data to api
 
 
 
+let cartTotal = document.getElementById('total-data').textContent;
 
 
-getCartTotals();
+async function postOrder() {
+
+    var firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let address = document.getElementById('address').value;
+    let country = document.getElementById('country').value;
+    let city = document.getElementById('city').value;
+    let postCode = document.getElementById('postCode').value;
+    let email = document.getElementById('email').value;
+
+    let products = [];
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        products.push(key);
+    }
+
+    let contact = {
+        lastName,
+        firstName,
+        address,
+        city,
+        email,
+    }
+    let order = {
+        contact,
+        products,
+    }
+    let jsonOrder = JSON.stringify(order);
+
+    const url = 'http://localhost:3000/api/cameras/order';
+    let http = new XMLHttpRequest();
+    http.onload = function () {
+        alert(http.responseText)
+    };
+
+    http.open("POST", url, true);
+    http.setRequestHeader('Content-Type', 'application/json');
+    http.send(jsonOrder);
+    console.log(jsonOrder);
+    console.log(http.responseText)
+};
+
+
+
 buildCartHTML();
-getCameraDetails();
-getCameraImage();
+
+
+
+var tdElem = document.getElementById('total-data');
+var tdText = tdElem.innerText | tdElem.textContent;
